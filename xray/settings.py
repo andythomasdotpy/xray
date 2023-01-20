bf15438e7cf8e4ffadbb11332fde8913ea21aefb
Andy <andythomas.py@gmail.com>
2023-01-20 15:48:04 -0600

    Added decouple before redeploy

--- xray/settings.py
+++ xray/settings.py
@@ -1,19 +1,19 @@
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-ie5c@^iqjyrs^&i7u!iwo4z1gc5zofal-gk=$$oc=oebsf!z3+"
+SECRET_KEY = config("MY_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["xray-env.eba-fczaaky6.us-west-2.elasticbeanstalk.com"]
+ALLOWED_HOSTS = [
+    "127.0.0.1",
+    "xray-env.eba-fczaaky6.us-west-2.elasticbeanstalk.com",
+    ]
 
 
 # Application definition
