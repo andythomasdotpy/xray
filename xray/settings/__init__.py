57c96cfc8594535b1f59110624d64cf37923b863
Andy <andythomas.py@gmail.com>
2023-01-21 15:21:44 -0600

    Updated __init__.py

--- xray/settings/__init__.py
+++ xray/settings/__init__.py
@@ -1,9 +1,7 @@
 from decouple import config
 
 try:
-    SETTINGS_VAR = config("SETTINGS_DEV_PROD")
-    print(SETTINGS_VAR)
-    if SETTINGS_VAR == "prod":
+    if config("SETTINGS_DEV_PROD") == "prod":
         from .prod import *
 except:
-        from .dev import *
+    from .dev import *
